d93ed73133bee7481cbc535d0c421e7b922d20b9
1wemonk <boringgtoreitup@gmail.com>
2024-05-30 14:02:33 +0300

    выгрузка бэкенда

---
+++ middllewares/cors.js
@@ -0,0 +1,21 @@
+// Вместо нашего адреса вставь свой
+// Можно ещё не удалять адреса с localhost, но мы удалили для краткости
+const allowedCors = ["https://pindiefrontsrrnt.nomoredomainswork.ru", 
+                     "http://pindiefrontsrrnt.nomoredomainswork.ru", 
+                     "http://localhost:3001", 
+                     "http://localhost:3000", 
+                     "https://pindiebacksrrnt.nomoredomainswork.ru/api",
+                     "https://pindiebacksrrnt.nomoredomainswork.ru"];
+
+function cors(req, res, next) {
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
+}
+
+module.exports = cors; 
